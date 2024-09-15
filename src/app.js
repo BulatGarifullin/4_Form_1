0188e3de31cc6b81f42c74ab53d8e1a30bf65ef8
BulatGarifullin <130639959+BulatGarifullin@users.noreply.github.com>
2024-09-15 19:59:42 +0300

    push

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import { Todo } from './components';
+
+export const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((todosData) => todosData.json())
+			.then((loadedTodos) => setTodos(loadedTodos));
+	}, []);
+
+	return (
+		<div className={styles.app}>
+			<div>
+				{todos.map(({ id, title, completed }) => (
+					<Todo key={id} title={title} completed={completed} />
+				))}
+			</div>
+		</div>
+	);
+};
